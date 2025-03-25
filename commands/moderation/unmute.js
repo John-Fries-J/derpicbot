667ee8c8c908dae75a2fe8d3cf6019238a160edc
John Fries <w.bababooie@gmail.com>
2025-03-25 02:27:46 +0000

    Upd: add unmute permissions

--- commands/moderation/unmute.js
+++ commands/moderation/unmute.js
@@ -1,14 +1,17 @@
 const { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits } = require('discord.js');
 const { green } = require('../../colors.json');
+const { roles } = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('unmute')
         .setDescription('Unmutes a user from the server.')
-        .setDefaultMemberPermissions(PermissionFlagsBits.ManageMessages)
         .setDMPermission(false)
         .addUserOption(option => option.setName('user').setDescription('The user to unmute').setRequired(true)),
     async execute(interaction) {
+        if (!interaction.member.permissions.has(PermissionFlagsBits.ManageRoles) && !interaction.member.roles.cache.has(roles.modRole)) {
+            return await interaction.reply({ content: 'You do not have permission to use this command.', ephemeral: true });
+        }
         const user = interaction.options.getUser('user');
         const member = await interaction.guild.members.fetch(user);
         member.timeout(null, 'Unmuted')
