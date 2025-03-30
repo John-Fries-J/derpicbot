fd98b86d10a4ac4637d3664fbd3573b04b155790
John Fries <w.bababooie@gmail.com>
2025-03-30 19:22:46 +0100

    Feat: Suggestions w/ Auto thread

---
+++ commands/suggestions/suggest.js
@@ -0,0 +1,40 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { EmbedBuilder } = require('@discordjs/builders');
+const { blue } = require('../../colors.json');
+const { suggestionID } = require('../../config.json');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('suggest')
+        .setDescription('Suggest a feature for the server.')
+        .addStringOption(option => option.setName('suggestion').setDescription('The suggestion you want to make.').setRequired(true)),
+    async execute(interaction) {
+        const suggestion = interaction.options.getString('suggestion');
+        const user = interaction.user;
+        const embed = new EmbedBuilder()
+            .setTitle(`Suggestion`)
+            .setDescription(`**Suggestion:**\n${suggestion}`)
+            .setThumbnail(user.displayAvatarURL({ dynamic: true }))
+            .setTimestamp()
+            .setColor(parseInt(blue.replace('#', ''), 16))
+            .setFooter({ text: 'Suggested by ' + user.tag });
+
+        try {
+            const suggestionChannel = await interaction.client.channels.fetch(suggestionID);
+            const message = await suggestionChannel.send({ embeds: [embed] });
+            
+            await message.react('✅'); 
+            await message.react('❌');
+            
+            await message.startThread({
+                name: `Suggestion by ${user.tag}`,
+                reason: 'Creating a thread for the suggestion.'
+            });
+            
+            await interaction.reply({ content: 'Your suggestion has been submitted!', ephemeral: true });
+        } catch (error) {
+            console.error('Error sending suggestion:', error);
+            await interaction.reply({ content: 'Failed to submit suggestion. Please try again later.', ephemeral: true });
+        }
+    },
+};
