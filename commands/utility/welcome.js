20e49db5778fbb96faa8745ec12a98ad7e6da310
John Fries <w.bababooie@gmail.com>
2025-03-30 14:25:58 +0100

    Fix: Welcome command. I'm stupid

--- commands/utility/welcome.js
+++ commands/utility/welcome.js
@@ -1,5 +1,6 @@
 const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { blue } = require('../../colors.json');
+const config = require('../../config.json');
 
 module.exports = {
     data: new SlashCommandBuilder()
@@ -7,30 +8,26 @@ module.exports = {
         .setDescription('Sends welcome embed reminding the user to read the rules.')
         .addUserOption(option => option.setName('user').setDescription('Ping the user with the message')),
     async execute(interaction) {
-        if (!$config.welcomeEmbed.title || !$config.welcomeEmbed.description || !$config.welcomeEmbed.footer || !$config.welcomeEmbed.thumbnail) {
+        if (!config.WelcomeEmbed.title || !config.WelcomeEmbed.description || !config.WelcomeEmbed.footer || !config.WelcomeEmbed.thumbnail) {
             interaction.reply({ content: 'The welcome embed is not set up properly. Please check the config file.', ephemeral: true });
             console.log('The welcome embed is not set up properly. Please check the config file.');
+            return;
         }
-        else if (!interaction.options.getUser('user')) {
-            const newembed = new EmbedBuilder()
-            .setTitle(`${config.welcomeEmbed.title}`)
-            .setDescription(`${config.welcomeEmbed.description}`)
-            .setFooter({ text: `${config.welcomeEmbed.footer}` })
-            .setThumbnail({ url: `${config.welcomeEmbed.thumbnail}` })
-            .setCiolor(blue);     
-            interaction.reply({ embeds: [welcomeEmbed] });
-        }
-        else if (interaction.options.getUser('user')) {
-            const user = interaction.options.getUser('user');
-            channel = interaction.guild.channels.cache.get(interaction.channelId);
-            const newembed = new EmbedBuilder()
-            .setTitle(`${config.welcomeEmbed.title}`)
-            .setDescription(`${config.welcomeEmbed.description}`)
-            .setFooter({ text: `${config.welcomeEmbed.footer}` })
-            .setThumbnail({ url: `${config.welcomeEmbed.thumbnail}` })
-            .setCiolor(blue);     
-            channel.send({content: `${user}`, embeds: [welcomeEmbed] });
+
+        const user = interaction.options.getUser('user');
+        const embed = new EmbedBuilder()
+            .setTitle(config.WelcomeEmbed.title)
+            .setDescription(config.WelcomeEmbed.description.replace('${user}', user ? `<@${user.id}>` : ''))
+            .setFooter({ text: config.WelcomeEmbed.footer })
+            .setThumbnail(config.WelcomeEmbed.thumbnail)
+            .setColor(blue);
+
+        if (!user) {
+            interaction.reply({ embeds: [embed] });
+        } else {
+            const channel = interaction.guild.channels.cache.get(interaction.channelId);
+            channel.send({ content: `${user}`, embeds: [embed] });
             interaction.reply({ content: `Welcome embed has been sent to ${user}`, ephemeral: true });
         }
     }
-}
+};
